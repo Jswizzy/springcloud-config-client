6b728559f533534d9f33a0df5106affeebb89eb4
Jswizzy <smithjustinn@hotmail.com>
2017-08-31 01:53:27 -0400

    add refresh

---
+++ readme.md
@@ -0,0 +1,5 @@
+# add JCE cypto to java
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt update
+sudo apt install oracle-java8-unlimited-jce-policy 
--- src/main/java/name/smithjn/springcloudconfigclient/RateController.java
+++ src/main/java/name/smithjn/springcloudconfigclient/RateController.java
@@ -1,11 +1,13 @@
 package name.smithjn.springcloudconfigclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RefreshScope
 public class RateController {
 
     @Value("${rate}")
@@ -17,12 +19,16 @@ public class RateController {
     @Value("${tollstart}")
     String tollstart;
 
+    @Value("${connstring}")
+    String connstring;
+
 
     @RequestMapping("/rate")
     public String getRate(Model model) {
         model.addAttribute("rateamount", rate);
         model.addAttribute("lanes", lanecount);
         model.addAttribute("tollstart", tollstart);
+        model.addAttribute("connstring", connstring);
 
         return "rateview";
     }
